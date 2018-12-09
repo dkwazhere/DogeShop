d40e00822fc237cadd1a520fbbe47875f3b5a545
dkwazhere <dhealth77@gmail.com>
2018-12-08 23:06:47 -0800

    fixed navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,32 +16,40 @@ const Home = () => {
           </Button>
         </Link>
       </div>
-      <Row className="show-grid text-center">
-        <Col xs={12} sm={4} className="shop-wrapper">
-          <Image src="assets/small-dog.jpg" circle className="dog-home-pic" />
-          <Link to="/about">
-            <Button className="dog-button" bsStyle="link" block="true">
-              Shop Small Dogs
-            </Button>
-          </Link>
-        </Col>
-        <Col xs={12} sm={4} className="shop-wrapper">
-          <Image src="assets/medium-dog.jpg" circle className="dog-home-pic" />
-          <Link to="/about">
-            <Button className="dog-button" bsStyle="link" block="true">
-              Shop Medium Dogs
-            </Button>
-          </Link>
-        </Col>
-        <Col xs={12} sm={4} className="shop-wrapper">
-          <Image src="assets/large-dog.jpg" circle className="dog-home-pic" />
-          <Link to="/about">
-            <Button className="dog-button" bsStyle="link" block="true">
-              Shop Large Dogs
-            </Button>
-          </Link>
-        </Col>
-      </Row>
+      <div className="dog-size-shop">
+        <h3 className="dog-size-header">Find your perfect dog today!</h3>
+        <p className="dog-size-paragraph">What's your preferred dog size?</p>
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={4} className="shop-wrapper">
+            <Image src="assets/small-dog.jpg" square className="dog-home-pic" />
+            <Link to="/about">
+              <Button className="dog-button" bsStyle="link" block="true">
+                Shop Small Dogs
+              </Button>
+            </Link>
+          </Col>
+          <Col xs={12} sm={4} className="shop-wrapper">
+            <Image
+              src="assets/medium-dog.jpg"
+              square
+              className="dog-home-pic"
+            />
+            <Link to="/about">
+              <Button className="dog-button" bsStyle="link" block="true">
+                Shop Medium Dogs
+              </Button>
+            </Link>
+          </Col>
+          <Col xs={12} sm={4} className="shop-wrapper">
+            <Image src="assets/large-dog.jpg" square className="dog-home-pic" />
+            <Link to="/about">
+              <Button className="dog-button" bsStyle="link" block="true">
+                Shop Large Dogs
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
