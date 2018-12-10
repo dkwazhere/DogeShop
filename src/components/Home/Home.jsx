af96e33ff3616f04e6263d103b72da27ec6b1123
dkwazhere <dhealth77@gmail.com>
2018-12-09 22:08:19 -0800

    added a about us section on the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -50,6 +50,39 @@ const Home = () => {
           </Col>
         </Row>
       </div>
+      <div className="about-us-section">
+        <h3 className="about-us-header">A little about us ...</h3>
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={3}>
+            <Image src="assets/trophy.png" className="about-us-pic" />
+            <p>
+              Our dogs are top quality and show dog worthy, 5 of 2000 dogs sold
+              were in the 2018 American Kennel Club show
+            </p>
+          </Col>
+          <Col xs={12} sm={3}>
+            <Image src="assets/internet.png" className="about-us-pic" />
+            <p>
+              We have an amazing shipping process. We offer world wide shipping
+              with a 1 day guarantee. We offer a 30 day return policy
+            </p>
+          </Col>
+          <Col xs={12} sm={3}>
+            <Image src="assets/price-tag.png" className="about-us-pic" />
+            <p>
+              Our prices amazing and affordable to every customer. Our prices
+              range from $10.00 to $9,000{" "}
+            </p>
+          </Col>
+          <Col xs={12} sm={3}>
+            <Image src="assets/plant.png" className="about-us-pic" />
+            <p>
+              We feed our dogs/puppies non-gmo food. Our company is anti gmo and
+              make our own dog food.
+            </p>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
