c9aa1fa78cb3349affb49e7bb97607c1288b6d5e
dkwazhere <dhealth77@gmail.com>
2018-12-10 23:10:31 -0800

    finished footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,36 +1,60 @@
 import React from "react";
-import { Col, Image, Button, Row } from "react-bootstrap";
+import { Col, Row, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <hr className="footer-hr" />
       <Row className="show-grid text-center">
         <Col xs={4} sm={2}>
-          <p>Shop</p>
-          <Link to="/smalldogs">Small Dogs</Link>
+          <p className="footer-p">Shop</p>
+          <Link to="/smalldogs" className="footer-links">
+            :: Small Dogs
+          </Link>
           <br />
-          <Link to="/mediumdogs">Medium Dogs</Link>
+          <Link to="/mediumdogs">:: Medium Dogs</Link>
           <br />
-          <Link to="/largedogs">Large Dogs</Link>
+          <Link to="/largedogs">:: Large Dogs</Link>
           <br />
-          <Link to="/accessories">Accessories</Link>
+          <Link to="/accessories">:: Accessories</Link>
+        </Col>
+        <Col xs={4} sm={3}>
+          <p className="footer-p">:: Support</p>
+          <Link to="/about">:: About Us</Link>
+          <br />
+          <Link to="/delivery">:: Delivery Rates</Link>
+          <br />
+          <Link to="/shipping">:: Shipping and Returns</Link>
+          <br />
+          <Link to="/jobs">:: Jobs</Link>
         </Col>
         <Col xs={4} sm={4}>
-          <p>Support</p>
-          <Link to="/about">About Us</Link>
-          <br />
-          <Link to="/delivery">Delivery Rates</Link>
-          <br />
-          <Link to="/shipping">Shipping and Returns</Link>
-          <br />
-          <Link to="/jobs">Jobs</Link>
+          <p className="footer-p">About us</p>
+          <p className="footer-about">
+            We are a dedicated dog shop who strive to please you and your
+            family. We offer the best show case dogs and offer reasonable
+            prices. We also sell high quality accessories for your dogs.
+          </p>
+          <a href="https://facebook.com" target="_blank">
+            <Image src="assets/facebook.png" className="footer-logo" />
+          </a>
+          <a href="https://instagram.com" target="_blank">
+            <Image src="assets/instagram.png" className="footer-logo" />
+          </a>
+          <a href="https://linkedin.com" target="_blank">
+            <Image src="assets/linkedin.png" className="footer-logo" />
+          </a>
+          <a href="https://pinterest.com" target="_blank">
+            <Image src="assets/pinterest.png" className="footer-logo" />
+          </a>
+          <a href="https://twitter.com" target="_blank">
+            <Image src="assets/twitter.png" className="footer-logo" />
+          </a>
         </Col>
-        <Col xs={4} sm={6}>
-          <p>Sign up for newsletters</p>
-          <p>
+        <Col xs={4} sm={3}>
+          <p className="footer-p">Sign up for newsletters</p>
+          <p className="footer-newsletter">
             Dont worry, we hate spam too - we will send you only the best stuff
           </p>
           <form>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -55,28 +55,28 @@ const Home = () => {
         <Row className="show-grid text-center">
           <Col xs={3} sm={3}>
             <Image src="assets/trophy.png" className="about-us-pic" />
-            <p>
+            <p className="about-us-p">
               Our dogs are top quality and show dog worthy, 5 of 2000 dogs sold
               were in the 2018 American Kennel Club show
             </p>
           </Col>
           <Col xs={3} sm={3}>
             <Image src="assets/internet.png" className="about-us-pic" />
-            <p>
+            <p className="about-us-p">
               We have an amazing shipping process. We offer world wide shipping
               with a 1 day guarantee. We offer a 30 day return policy
             </p>
           </Col>
           <Col xs={3} sm={3}>
             <Image src="assets/price-tag.png" className="about-us-pic" />
-            <p>
+            <p className="about-us-p">
               Our prices amazing and affordable to every customer. Our prices
               range from $10.00 to $9,000{" "}
             </p>
           </Col>
           <Col xs={3} sm={3}>
             <Image src="assets/plant.png" className="about-us-pic" />
-            <p>
+            <p className="about-us-p">
               We feed our dogs/puppies non-gmo food. Our company is anti gmo and
               make our own dog food.
             </p>
