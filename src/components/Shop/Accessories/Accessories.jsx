e632568fe0bfeccb4e187b0264ee848c9bd31669
dkwazhere <dhealth77@gmail.com>
2018-12-12 14:26:03 -0800

    added accessories shop

--- src/components/Shop/Accessories/Accessories.jsx
+++ src/components/Shop/Accessories/Accessories.jsx
@@ -1,10 +1,161 @@
 import React from "react";
+import { Col, Button, Panel, Row, Image } from "react-bootstrap";
 import "./Accessories.css";
 
 const Home = () => {
   return (
     <div className="accessories">
-      <h1>Accessories</h1>
+      <h3>Mid Sized Dogs</h3>
+      <Row className="show-grid text-center">
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/avocado-toy.jpg"
+                className="accessories-pic"
+              />
+              <p>Avocado Toy with ball</p>
+              <p>$12.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/bark-trainer-collar.jpg"
+                className="accessories-pic"
+              />
+              <p>Bark Trainer Collar</p>
+              <p>$89.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/blue-collar.jpg"
+                className="accessories-pic"
+              />
+              <p>Blue Collar</p>
+              <p>$9.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/burgandy-dog-bed.jpg"
+                className="accessories-pic"
+              />
+              <p>Burgandy Dog Bed</p>
+              <p>$59.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+      </Row>
+      <Row className="show-grid text-center">
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/dog-bone-toy.jpg"
+                className="accessories-pic"
+              />
+              <p>Dog Bone Squeeze Toy</p>
+              <p>$21.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/dog-toy.jpg"
+                className="accessories-pic"
+              />
+              <p>Squueze Toy</p>
+              <p>$39.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/large-cage.jpg"
+                className="accessories-pic"
+              />
+              <p>Cage for large dogs</p>
+              <p>$59.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/medium-cage.jpg"
+                className="accessories-pic"
+              />
+              <p>Cage for Medium dogs</p>
+              <p>$49.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+      </Row>
+      <Row className="show-grid text-center">
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/small-cage.jpg"
+                className="accessories-pic"
+              />
+              <p>Cage for Small dogs</p>
+              <p>$39.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/navy-pattern-dog-bed.jpg"
+                className="accessories-pic"
+              />
+              <p>Navy Patterned Dog Bed</p>
+              <p>$39.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/pet-tag.jpg"
+                className="accessories-pic"
+              />
+              <p>Dog Tag</p>
+              <p>$9.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+        <Col xs={12} sm={3} className="shop-wrapper">
+          <Panel>
+            <Panel.Body>
+              <Image
+                src="assets/accessories/retractable-leash.jpg"
+                className="accessories-pic"
+              />
+              <p>Retractable Leash</p>
+              <p>$39.99</p>
+            </Panel.Body>
+          </Panel>
+        </Col>
+      </Row>
     </div>
   );
 };
